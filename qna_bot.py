a8c9c7a29873cb108707ec044c9921443785ce3b
Harsh7114 <harshrocking235@gmail.com>
2026-04-05 00:57:33 +0530

    done

--- qna_bot.py
+++ qna_bot.py
@@ -8,13 +8,28 @@ llm = ChatGoogleGenerativeAI(model ="gemini-3-flash-preview")
 
 st.title("🤖 AskBuddy ~ AI QnA Bot")
 st.markdown("My QnA bot with Langchain and Google Gemini")
+
+# to make chat window retain in UI\
+if "messages" not in st.session_state:
+    st.session_state.messages =[]
+
+for message in st.session_state.messages:
+    role= message["role"]
+    content=message["content"]
+    st.chat_message(role).markdown(content)
+
+
+
+
 #chat window
 query = st.chat_input("Ask Any Question:")
 if query:
+   st.session_state.messages.append({"role":"user","content":query})
+
    st.chat_message("user").markdown(query)
    res= llm.invoke(query)
    st.chat_message("ai").markdown(res.content[0]["text"])
-
+   st.session_state.messages.append({"role":"ai","content":res.content[0]["text"]})
 if False:
     """while True :
     query = input("User:")
