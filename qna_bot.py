1c6801c48f961661d6bdce627156c4722fe05b0b
Harsh7114 <harshrocking235@gmail.com>
2026-04-04 23:47:25 +0530

    initial commit

---
+++ qna_bot.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_core.output_parsers import StrOutputParser
+parser = StrOutputParser()
+from langchain_google_genai import ChatGoogleGenerativeAI
+llm = ChatGoogleGenerativeAI(model ="gemini-3-flash-preview")
+
+while True :
+    query = input("User:")
+
+    if query.lower() in ["quit","exit","bye"]:
+        print ("GoodBYE >")
+        break;
+
+    res= llm.invoke(query)
+    print("AI:",res.content[0]["text"])
+
+#question = "who is pm of india?"
+
+#res = llm.invoke(question)
+#print(parser.invoke(res))
+
+
